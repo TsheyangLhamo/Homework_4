777a584c88148113e0b9c674f2b9e3c67b597a97
TsheyangLhamo <tsheyanglhamo77@gmail.com>
2022-07-22 16:05:48 -0400

    hw4

--- Assets/Scripts/Problem_4.cs
+++ Assets/Scripts/Problem_4.cs
@@ -12,7 +12,12 @@ public class Problem_4 : MonoBehaviour
     //You should have the results of Part_4, 0,0,0, Part_4_Child -5,-5,-5 and Part_4_GameObject, 5,5,5
     void Start()
     {
-
+        Transform v = GetComponent<Transform>();
+        PrintOutNameAndPosition(v.name, v.position);
+        Transform t= transform.GetChild(0).GetComponent<Transform>();
+        PrintOutNameAndPosition(t.name, t.position);
+        Transform z = GameObject.Find("Part_4_GameObject").GetComponent<Transform>();
+        PrintOutNameAndPosition(z.name, z.position);
     }
 
     //Prints out the name and the position of the variables
